120b93b8a2cccd6047fc98eee3291dba4101df43
kyle <nojaim.k@gmail.com>
2024-08-15 11:11:11 -0400

    avoid looping an array to find opposite number in reversed evens vs odds

---
+++ addressBook/js/main.js
@@ -0,0 +1,21 @@
+function overTheRoad(address, n){
+    return 2 * n + 1 - address
+  //   let arrOfOdds = []
+  //   let arrOfEvens = []
+  //   for (let i = 1 ; i <= n * 2 ; i ++){
+  //     if (i % 2 === 1){
+  //       arrOfOdds.push(i)
+  //     }else{
+  //       arrOfEvens.push(i)
+  //     }
+  //   }
+  //   arrOfEvens = arrOfEvens.reverse()
+  //   if (address % 2 === 1){
+  //     let index = arrOfOdds.indexOf(address)
+  //     return arrOfEvens[index]
+  //   }else {
+  //     let index = arrOfEvens.indexOf(address)
+  //     return arrOfOdds[index]
+  //   }
+  //   lololololololololol
+  }
