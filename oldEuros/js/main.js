ad4b4511cf2e5bbdff02c9cf391ba0648337adba
kyle <nojaim.k@gmail.com>
2024-08-21 10:32:21 -0400

    simple comparison and element placement

---
+++ oldEuros/js/main.js
@@ -0,0 +1,11 @@
+function uefaEuro2016(teams, scores){
+    let winner = ''
+    if (scores[0] > scores[1]){
+      winner = teams[0] + ' won!'
+    }else if (scores[1] > scores[0]){
+      winner = teams[1] + ' won!'
+    }else{
+      winner = 'teams played draw.'
+    }
+    return `At match ${teams[0]} - ${teams[1]}, ${winner}`
+  }
