9e888aa4336bb015784f19682f26f895d34d4476
kyle <nojaim.k@gmail.com>
2024-08-23 13:55:37 -0400

    issues with longer than maze.length

--- runTheMaze/js/main.js
+++ runTheMaze/js/main.js
@@ -1,7 +1,7 @@
 function mazeRunner(maze, directions) {
     let first = -1
     let second = -1
-  
+    let offBoard = false
     for (let i = 0 ; i < maze.length ; i++){
       if (maze[i].includes(2)){
           first = i
@@ -17,10 +17,24 @@ function mazeRunner(maze, directions) {
       if (directions[i] === 'S') {first++}
       if (directions[i] === 'E') {second++}
       if (directions[i] === 'W') {second--}
-      console.log(first,second)
+      if (first > maze.length - 1 || first < 0){
+        offBoard = true
+      }
+      if (second > maze[first].length - 1 || second < 0){
+        offBoard = true
+      }
       placeOnBoard.push(maze[first][second])
+      if (maze[first][second] === 3){
+      }
+      if (maze[first][second] === 1){
+      }
+
     }
-    console.log(placeOnBoard)
+    if (offBoard) return 'dead'
+    if (placeOnBoard.includes(1)) return 'dead'
+    if (placeOnBoard.includes(3)) return 'alive'
+    if (placeOnBoard.every(el => el === 0)) return 'lost'
+    return placeOnBoard
   }
 
   var maze = [[1,1,1,1,1,1,1],
@@ -34,3 +48,8 @@ function mazeRunner(maze, directions) {
 console.log(mazeRunner(maze,["N","N","N","N","N","E","E","E","E","E"]))
 console.log(mazeRunner(maze,["N","N","N","N","N","E","E","S","S","E","E","N","N","E"]))
 console.log(mazeRunner(maze,["N","N","N","N","N","E","E","E","E","E","W","W"]))
+
+console.log(mazeRunner(maze,["N","N","N","W","W"]))
+console.log(mazeRunner(maze,["N","N","N","N","N","E","E","S","S","S","S","S","S"]))
+
+console.log(mazeRunner(maze,["N","E","E","E","E"]))
