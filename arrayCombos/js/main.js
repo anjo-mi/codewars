3601926b3694f90539897c068f3b5259abe8c3cd
kyle <nojaim.k@gmail.com>
2024-09-20 19:53:22 -0400

    added test cases

--- arrayCombos/js/main.js
+++ arrayCombos/js/main.js
@@ -11,3 +11,11 @@ function createDict(keys, vals){
 console.log(createDict(['a', 'b', 'c'], [1, 2, 3]), {'a': 1, 'b': 2, 'c': 3})
 console.log(createDict(['a', 'b', 'c'], [1, 2, 3, 4]), {'a': 1, 'b': 2, 'c': 3})
 console.log(createDict(['a', 'b', 'c','d'], [1, 2, 3]), {'a': 1, 'b': 2, 'c': 3, 'd':null})
+
+console.log(createDict(['a', 'b', 'c'], [1, 2, 3]), {'a': 1, 'b': 2, 'c': 3})
+console.log(createDict([], []), {}, "empty keys arguments should return '{}'")
+console.log(createDict([], [1]), {}, "empty keys arguments should return '{}'")
+console.log(createDict(['a', 'b'], [1, 2, 3, 4]), {'a': 1, 'b': 2},"if not enough keys ignore rest of values")
+console.log(createDict(['a'], []), {'a': null}, " if not enough values, value is 'null'")
+console.log(createDict(['a', 'b', 'c', 'd', 'e'], [1, 2]), {'a': 1, 'b': 2, 'c': null, 'd': null, 'e': null}, "if not enough values, value is 'null' . . . ")
+console.log(createDict(['a', 'b', 'c', 'd', 'e'], [1, 2, 0, false, ""]), {'a': 1, 'b': 2, 'c': 0, 'd': false, 'e':""}, "'0', 'false' or`\"\" (an empty string) are valid values")
