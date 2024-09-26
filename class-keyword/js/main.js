a359a22a12acd0bd3dff64b1c3758d9432a349ab
kyle <nojaim.k@gmail.com>
2024-09-25 21:23:32 -0400

    overall functionality works, bugs to work out on fringe cases

--- class-keyword/js/main.js
+++ class-keyword/js/main.js
@@ -11,32 +11,47 @@ class KeywordCipher{
         a[el] = a[el] || ( alpha.indexOf(el) + 1 )
         return a
       } , {} )
+
+    //   console.log(alpha,word,keyedBeta)
       for ( let key in keyedBeta ){
         if (alpha.includes(key)){
-          alpha = alpha.splice( alpha.indexOf(key) , 1)
+          let letter = alpha.splice( alpha.indexOf(key) , 1)
         }
       }
-      for ( let i = word.length - 1 ; i <= 0 ; i-- ){
-        alpha.unshift(word[i])
-      }
+      alpha = word.concat(alpha)
       return alpha
     }
     
     encode(str){
-      let coded = this.createCode
+      let coded = this.createCode()
+      str = str.split('')
+      for (let i = 0 ; i < str.length ; i++){
+        let index = this.abc.indexOf(str[i])
+        str[i] = coded[index]
+      }
+      return str.join('')
         
     }
     
     decode(str){
+        let coded = this.createCode()
+        str = str.split('')
+        for (let i = 0 ; i < str.length ; i++){
+            let index = coded.indexOf(str[i])
+            str[i] = this.abc[index]
+        }
+        return str.join('')
       
     }
   }
 
-  var abc = "abcdefghijklmnopqrstuvwxyz";
-var key = "keyword";
+let abc = "abcdefghijklmnopqrstuvwxyz";
+let key = "keyword";
 var cipher = new KeywordCipher(abc, key);
 
 console.log(cipher.encode("abc"), "key");
 console.log(cipher.encode("xyz"), "vxz");
 console.log(cipher.decode("key"), "abc");
 console.log(cipher.decode("vxz"), "xyz");
+
+console.log(cipher.createCode())
