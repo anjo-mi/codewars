43adbd9a360dfe5c83b615c1dc598bdb2fb8344a
kyle <nojaim.k@gmail.com>
2024-11-12 21:49:24 -0500

    access args with return function

---
+++ functions-combine-two/js/main.js
@@ -0,0 +1,15 @@
+function compose(f,g) {
+    return function (...args){
+      return f(g(...args))
+    }
+  }
+
+
+  let add1 = function(a){return a + 1}
+  const id = function(a){return a}
+  console.log( compose(add1,id)(0), 1 )
+  
+  add1 = function(a){return a + 1}
+  const addAll3 = function(a,b,c){return a + b + c}
+  console.log( compose(add1,addAll3)(1,2,3), 7 )
+  
