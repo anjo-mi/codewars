24f1c1c80ffc00036c55af8efe0c3b1cdb504c31
kyle <nojaim.k@gmail.com>
2025-01-17 13:12:34 -0500

    determine screen height given ratio and width

---
+++ screen-height-from-ratio/js/main.js
@@ -0,0 +1,13 @@
+function findScreenHeight(width, ratio) {
+    let [w,h] = ratio.split(':');
+    w = parseInt(w);
+    h = parseInt(h);
+    const mult = width / w;
+    let newHeight = Math.trunc(h * mult);
+    return `${width}x${newHeight}`
+  }
+
+
+console.log(findScreenHeight(1024,"4:3"), "1024x768");
+console.log(findScreenHeight(1280,"16:9"), "1280x720");
+console.log(findScreenHeight(3840,"32:9"), "3840x1080");
