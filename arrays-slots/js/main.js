188b6ec0e2bca600a466e05e2b6cb637d0d208cb
kyle <nojaim.k@gmail.com>
2025-01-20 15:45:38 -0500

    added test cases, working as intended

--- arrays-slots/js/main.js
+++ arrays-slots/js/main.js
@@ -24,3 +24,25 @@ function fruit(arrs, spin){
     if (count['Wild'] === 1 && points) points *= 2;
     return points * mult;
 }
+
+let reel = ["Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"];
+let spin = [0,0,0];
+console.log(fruit([reel,reel,reel],spin),100, "Should return: '100'");
+
+let reel1 = ["Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"];
+let reel2 = ["Bar", "Wild", "Queen", "Bell", "King", "Seven", "Cherry", "Jack", "Star", "Shell"];
+let reel3 = ["Bell", "King", "Wild", "Bar", "Seven", "Jack", "Shell", "Cherry", "Queen", "Star"];
+spin = [5,4,3];
+console.log(fruit([reel1,reel2,reel3],spin),0, "Should return: '0'");
+
+reel1 = ["King", "Cherry", "Bar", "Jack", "Seven", "Queen", "Star", "Shell", "Bell", "Wild"];
+reel2 = ["Bell", "Seven", "Jack", "Queen", "Bar", "Star", "Shell", "Wild", "Cherry", "King"];
+reel3 = ["Wild", "King", "Queen", "Seven", "Star", "Bar", "Shell", "Cherry", "Jack", "Bell"];
+spin = [0,0,1];
+console.log(fruit([reel1,reel2,reel3],spin),3, "Should return: '3'");
+
+reel1 = ["King", "Jack", "Wild", "Bell", "Star", "Seven", "Queen", "Cherry", "Shell", "Bar"];
+reel2 = ["Star", "Bar", "Jack", "Seven", "Queen", "Wild", "King", "Bell", "Cherry", "Shell"];
+reel3 = ["King", "Bell", "Jack", "Shell", "Star", "Cherry", "Queen", "Bar", "Wild", "Seven"];
+spin = [0,5,0];
+console.log(fruit([reel1,reel2,reel3],spin),6, "Should return: '6'");
