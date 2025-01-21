4168e7305abf71062989d9f33b136e882e874bc4
kyle <nojaim.k@gmail.com>
2025-01-21 12:22:08 -0500

    plus test cases

--- array-even-occurances/js/main.js
+++ array-even-occurances/js/main.js
@@ -5,3 +5,10 @@ function oddOnesOut(nums) {
     }, {})
     return nums.filter(el => count[el] % 2 === 0)
   }
+
+
+    console.log(oddOnesOut([1, 2, 3, 1, 3, 3]), [1, 1]);
+    console.log(oddOnesOut([75, 68, 75, 47, 68]), [75, 68, 75, 68]);
+    console.log(oddOnesOut([42, 72, 32, 4, 94, 82, 67, 67]), [67, 67]);
+    console.log(oddOnesOut([100, 100, 5, 5, 100, 50, 68, 50, 68, 50, 68, 5, 100]), [100, 100, 100, 100]);
+    console.log(oddOnesOut([82, 86, 71, 58, 44, 79, 50, 44, 79, 67, 82, 82, 55, 50]), [44, 79, 50, 44, 79, 50]);
