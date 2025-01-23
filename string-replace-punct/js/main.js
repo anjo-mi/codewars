8d55d9ace10eab5f1bb2cb049b1356ce49b8938a
kyle <nojaim.k@gmail.com>
2025-01-23 17:06:02 -0500

    added test cases

--- string-replace-punct/js/main.js
+++ string-replace-punct/js/main.js
@@ -8,3 +8,8 @@ function borrow(s){
             .replaceAll(',','')
             .toLowerCase()
   }
+
+
+  console.log(borrow('WhAt! FiCK! DaMn CAke?'), 'whatfickdamncake');
+  console.log(borrow('THE big PeOpLE Here!!'), 'thebigpeoplehere'); 
+  console.log(borrow('i AM a TINY BoY!!'), 'iamatinyboy'); 
