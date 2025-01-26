f6838c8c6f2b5bcaaac49d24cbaddc754311d53d
kyle <nojaim.k@gmail.com>
2025-01-26 15:20:30 -0500

    added test cases

--- class-protoype-bark/js/main.js
+++ class-protoype-bark/js/main.js
@@ -1,1 +1,9 @@
 Dog.prototype.bark = () => 'bark bitch!'
+
+  console.log('Can you make newly created dogs bark?');
+  var apollo = new Dog('Apollo', 'Dobermann', 'male', '4');
+  var zeus = new Dog('Zeus', 'Dobermann', 'male', '4');
+  
+  console.log(apollo.bark(), 'Woof!');
+  console.log(zeus.bark(), 'Woof!');
+  
