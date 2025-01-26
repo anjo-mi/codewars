f123149e9864036de5b1610a5105830659fe8444
kyle <nojaim.k@gmail.com>
2025-01-26 13:15:47 -0500

    provided simple test cases, upper lower case would be good addition

--- class-dictionary/js/main.js
+++ class-dictionary/js/main.js
@@ -12,4 +12,18 @@ class Dictionary {
     }
   }
 
+    let d = new Dictionary();
+  
+      d.newEntry('Apple', 'A fruit');
+      console.log(d.look('Apple'), 'A fruit');
+  
+      d.newEntry('Soccer', 'A sport');
+      console.log(d.look('Soccer'), 'A sport');
+    
+      console.log(d.look('Hi'), 'Can\'t find entry for Hi');
+      console.log(d.look('Ball'), 'Can\'t find entry for Ball');
+  
+      console.log(d.look('soccer'), 'Can\'t find entry for soccer');
+      d.newEntry('soccer', 'a sport');
+      console.log(d.look('soccer'), 'a sport');
   
