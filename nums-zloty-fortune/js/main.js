cb45379aba23857e42243e2dc1df7feb5a2e9fee
kyle <nojaim.k@gmail.com>
2025-01-26 15:59:52 -0500

    determin possible total amounts of money based on possible purchases

---
+++ nums-zloty-fortune/js/main.js
@@ -0,0 +1,23 @@
+function howMuch(m,n){
+    // $ = 9c + 1
+    // $ = 7b + 2
+    // 9c + 1 = 7b + 2
+    // 9c - 7b = 1
+    const min = m >= n ? n : m;
+    const max = m === min ? n : m;
+      console.log(max,min)
+    const possible = [];
+    for (let i = min; i <= max; i++){
+        const c = (i - 1) / 9;
+        const b = (i - 2) / 7;
+      console.log({c,b})
+        if (Number.isInteger(c) && Number.isInteger(b)){
+            possible.push({
+                M: i,
+                B: b,
+                C: c
+            })
+        }
+    }
+    return possible
+}
