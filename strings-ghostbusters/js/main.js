7bfcc1cbffb41a6930f878cceb180c3278bb479a
kyle <nojaim.k@gmail.com>
2025-01-27 13:28:45 -0500

    tests

--- strings-ghostbusters/js/main.js
+++ strings-ghostbusters/js/main.js
@@ -5,3 +5,7 @@ function ghostBusters(b) {
     }
     return b
   }
+
+console.log(ghostBusters("Factor y") === "Factory", 'Nope, there may still be a ghost in the building. Try again.');
+console.log(ghostBusters("O  f fi ce") === "Office", 'Nope, there may still be a ghost in the building. Try again.');
+console.log(ghostBusters("BusStation") === "You just wanted my autograph didn't you?", 'Nope, as there were no ghosts in the BusStation you need to return a witty retort.');
