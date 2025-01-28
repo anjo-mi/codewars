1c01457fa4fe543ada5009034f7358be9d591681
kyle <nojaim.k@gmail.com>
2025-01-27 20:06:57 -0500

    convert string to its reversed binary plus test cases

--- str-wrong-copy/js/main.js
+++ str-wrong-copy/js/main.js
@@ -9,3 +9,7 @@ function broken(x){
     }
     return wrongCopy
   }
+
+  console.log(broken("1"), "0");
+  console.log(broken("10000000101101111110011001000"), "01111111010010000001100110111");
+  console.log(broken("100010"), "011101");
