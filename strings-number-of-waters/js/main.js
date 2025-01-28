34570782d4eb761d0d11537f9db11830cb94cddd
kyle <nojaim.k@gmail.com>
2025-01-28 17:25:58 -0500

    pull nums from a string and reduce plus test cases

--- strings-number-of-waters/js/main.js
+++ strings-number-of-waters/js/main.js
@@ -13,3 +13,8 @@ function hydrate(s) {
     const drinks = nums.reduce((a,el) => a + el, 0)
     return `${drinks} ${drinks === 1 ? 'glass' : 'glasses'} of water`
   }
+
+
+  console.log(hydrate("1 beer"), "1 glass of water");
+  console.log(hydrate("2 glasses of wine and 1 shot"), "3 glasses of water");
+  console.log(hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"), "10 glasses of water");
