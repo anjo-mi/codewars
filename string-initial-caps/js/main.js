5fc9f716952da6b3a02f6afef17bb0921491718b
kyle <nojaim.k@gmail.com>
2025-01-28 20:43:53 -0500

    get intials and full last from name, capitalize

--- string-initial-caps/js/main.js
+++ string-initial-caps/js/main.js
@@ -1,3 +1,15 @@
+// A person's full name is usually composed of a first name, middle name and last name; however in some cultures (for example in South India) there may be more than one middle name.
+
+// Write a function that takes the full name of a person, and returns the initials, separated by dots ('.'). The initials must be uppercase. The last name of the person must appear in full, with its first letter in uppercase as well.
+
+// See the pattern below:
+
+// "code wars"            ---> "C.Wars"
+// "Barack hussein obama" ---> "B.H.Obama"
+// Names in the full name are separated by exactly one space (' ' ) ; without leading or trailing spaces. Names will always be lowercase, except optionally their first letter.
+
+
+
 function initials(n){
     n = n.split(' ');
     let last = n.pop();
@@ -6,3 +18,8 @@ function initials(n){
     n.push(last)
     return n.join('.')
   }
+
+
+  console.log('code wars', 'C.Wars');
+  console.log('Barack hussain obama', 'B.H.Obama');
+  console.log('barack hussain Obama', 'B.H.Obama');
