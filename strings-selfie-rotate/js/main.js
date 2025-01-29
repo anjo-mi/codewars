b1b91dcc419c697494811efdfbeeb3c00f351981
kyle <nojaim.k@gmail.com>
2025-01-29 13:44:12 -0500

    rot to rotate characters in each line, selfie to add dots per length, combine

--- strings-selfie-rotate/js/main.js
+++ strings-selfie-rotate/js/main.js
@@ -15,3 +15,16 @@ function rot(str) {
   function oper(fct, s) {
     return fct(s)
   }
+
+
+  function testIt(func, input, expected, funcName) {
+    it(`Function: ${funcName}\nInput string: ${JSON.stringify(input)}\nExpected: ${JSON.stringify(expected)} `, function() {
+        console.log(oper(func, input), expected);
+    });
+}
+
+    testIt(rot, "fijuoo\nCqYVct\nDrPmMJ\nerfpBA\nkWjFUG\nCVUfyL", "LyfUVC\nGUFjWk\nABpfre\nJMmPrD\ntcVYqC\nooujif", 'rot');
+    testIt(rot, "rkKv\ncofM\nzXkh\nflCB", "BClf\nhkXz\nMfoc\nvKkr", 'rot');
+
+    testIt(selfieAndRot, "xZBV\njsbS\nJcpN\nfVnP", "xZBV....\njsbS....\nJcpN....\nfVnP....\n....PnVf\n....NpcJ\n....Sbsj\n....VBZx", 'selfieAndRot');
+    testIt(selfieAndRot, "uLcq\nJkuL\nYirX\nnwMB", "uLcq....\nJkuL....\nYirX....\nnwMB....\n....BMwn\n....XriY\n....LukJ\n....qcLu", 'selfieAndRot');
