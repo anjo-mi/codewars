1511c5b5ec5b2f8c37d878c7fdcf12cb6fff8431
kyle <nojaim.k@gmail.com>
2025-01-30 13:36:29 -0500

    sort thru santas list to see what matches a list of children, case sensitive

--- arrays-santas-list/js/main.js
+++ arrays-santas-list/js/main.js
@@ -6,3 +6,7 @@ function findChildren(santasList, children) {
       return a
     }, []).sort()
   }
+
+  console.log(findChildren(["Jason", "Jackson", "Jordan", "Johnny"], ["Jason", "Jordan", "Jennifer"]), ["Jason", "Jordan"]);
+  console.log(findChildren(["jASon", "JAsoN", "JaSON", "jasON"], ["JasoN", "jASOn", "JAsoN", "jASon", "JASON"]), ["JAsoN", "jASon"]);
+  console.log(findChildren(["Jason", "James", "Johnson"], ["Jason", "James", "JJ"]), ["James", "Jason"]);
