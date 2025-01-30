b492930e534c6e3163a2ba0172fce6fd9fb09ec1
kyle <nojaim.k@gmail.com>
2025-01-30 12:27:18 -0500

    decode list of nums with a key to create message

--- nums-decode-array/js/main.js
+++ nums-decode-array/js/main.js
@@ -4,3 +4,6 @@ function decode  (code, n) {
     code = code.map((el,i) => el - n[i % n.length]);
     return code.map(el => alpha[el]).join('')
   }
+
+  console.log( decode([ 20, 12, 18, 30, 21], 1939), "scout" );
+  console.log( decode([ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8], 1939) , "masterpiece");
