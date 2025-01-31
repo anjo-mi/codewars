a6aca49b8dc8cbc18ace07c112fda273a61bc7b9
kyle <nojaim.k@gmail.com>
2025-01-31 08:29:13 -0500

    count how many times the sign changes thru an array of nums, not working with 0s rn

--- nums-array-sign-change/js/main.js
+++ nums-array-sign-change/js/main.js
@@ -1,3 +1,23 @@
+// Count how often sign changes in array.
+
+// result
+// number from 0 to ... . Empty array returns 0
+
+// example
+// const arr = [1, -3, -4, 0, 5];
+
+/*
+| elem | count |
+|------|-------|
+|  1   |  0    |
+| -3   |  1    |
+| -4   |  1    |
+|  0   |  2    |
+|  5   |  2    |
+*/
+
+// catchSignChange(arr) == 2;
+
 function catchSignChange(arr) {
     if (arr.length === 0) return 0;
     let alt = 0;
