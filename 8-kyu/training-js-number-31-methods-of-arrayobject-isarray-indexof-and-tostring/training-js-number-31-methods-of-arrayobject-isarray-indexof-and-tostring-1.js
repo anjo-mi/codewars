9a0a8d403deb0788065ba2a7c118f6c752e52c17
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-03 18:15:36 -0500

    Add solution - CodeHub

---
+++ 8-kyu/training-js-number-31-methods-of-arrayobject-isarray-indexof-and-tostring/training-js-number-31-methods-of-arrayobject-isarray-indexof-and-tostring-1.js
@@ -0,0 +1,6 @@
+ 
+function blackAndWhite(arr){
+  if (!Array.isArray(arr)) return `It's a fake array`
+  return arr.includes(5) && arr.includes(13) ? `It's a black array` : `It's a white array`
+  
+}
