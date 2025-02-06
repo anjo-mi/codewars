54cfc32cb04e04fb5fe6599e3c079bca436f4b4b
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-05 20:40:05 -0500

    Add solution - CodeHub

---
+++ 7-kyu/especially-joyful-numbers/especially-joyful-numbers-1.js
@@ -0,0 +1,7 @@
+ 
+function numberJoy(n) {
+  let num = n.toString().split('').map(Number);
+  const total = num.reduce((a,el) => a + el, 0);
+  const reverse = +(total.toString().split('').reverse().join(''));
+  return n % total === 0 && total * reverse === n;
+}
