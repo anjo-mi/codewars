b2a68fae8d31fa9cefe7fc313afac02b97346b3f
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-06 23:57:14 -0500

    Update solution - CodeHub

--- 7-kyu/string-reordering/string-reordering.js
+++ 7-kyu/string-reordering/string-reordering.js
@@ -1,13 +1,6 @@
-function sentence(objs) {
-  let order = objs.reduce((a,el) => {
-    a.push(Object.keys(el))
-    return a
-  }, []).map(el => Number(el[0]));
-  order = order.sort((a,b) => a-b);
-  const words = [];
-  for (let i = 0 ; i < order.length ; i++){
-    const word = objs.find(obj => obj.hasOwnProperty(order[i]));
-    words.push(word[order[i]]);
-  }
-  return words.join(' ');
+function sentence(list) {
+  return list
+    .sort((a, b) => Object.keys(a)[0] - Object.keys(b)[0])
+    .map(item => Object.values(item)[0])
+    .join(' ')
 }
