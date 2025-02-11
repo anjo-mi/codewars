a1ee8e84480514a391e866f03e1cdd854093aec6
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-11 16:39:55 -0500

    Update trimming-a-string.js

--- 7-kyu/trimming-a-string/trimming-a-string.js
+++ 7-kyu/trimming-a-string/trimming-a-string.js
@@ -1,5 +1,4 @@
- 
 function trim(str, size) {
-  return size < 3 ? str.slice(0,size) + '...' :
-         size > str.length ? str : str.slice(0, size - 3) + '...';
+  if (str.length <= size) return str;
+  return size <= 3 ? str.slice(0,size) + '...' : str.slice(0, size - 3) + '...';
 }
