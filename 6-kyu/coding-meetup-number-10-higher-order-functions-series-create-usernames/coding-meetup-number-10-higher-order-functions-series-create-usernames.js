316c385dbfc9e841378d76bf4e82d02bb7211294
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-12 17:16:02 -0500

    Return object from generateUserName

--- 6-kyu/coding-meetup-number-10-higher-order-functions-series-create-usernames/coding-meetup-number-10-higher-order-functions-series-create-usernames.js
+++ 6-kyu/coding-meetup-number-10-higher-order-functions-series-create-usernames/coding-meetup-number-10-higher-order-functions-series-create-usernames.js
@@ -1,8 +1,8 @@
- 
 function addUsername(list) {
   function generateUserName(obj){
     const birthYear = new Date().getFullYear() - obj.age;
-    obj.username = `${obj.firstName.toLowerCase()}${obj.lastName[0].toLowerCase()}${birthYear}`
+    obj.username = `${obj.firstName.toLowerCase()}${obj.lastName[0].toLowerCase()}${birthYear}`;
+    return obj;
   }
   return list.map(generateUserName);
 }
