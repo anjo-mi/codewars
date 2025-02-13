f741ab974d455c5f3132aec388365a015cdbd420
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-12 21:16:27 -0500

    Add solution - CodeHub

---
+++ 7-kyu/even-odd-disparity/even-odd-disparity.js
@@ -0,0 +1,16 @@
+ 
+function solve(a){
+  const totals = a.reduce((acc,el) => {
+    if (typeof el === 'number'){
+      if (el % 2 === 0){
+        acc['even'] = (acc['even'] || 0) + 1;
+      }else{
+        acc['odd'] = (acc['odd'] || 0) + 1;
+      }
+    }
+    return acc;
+  }, {})
+  if (!totals['even']) totals.even = 0;
+  if (!totals['odd']) totals.odd = 0;
+  return totals.even - totals.odd;
+};
