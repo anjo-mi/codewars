f209b561fc6816df04bcf5dbe700ee0d5b04e867
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-14 00:24:28 -0500

    fixed exceeding call stack issue

--- 7-kyu/doubleton-number/doubleton-number.ts
+++ 7-kyu/doubleton-number/doubleton-number.ts
@@ -1,13 +1,16 @@
- 
 export function doubleton(num: number): number {
-  let possDub: number = num + 1;
-  const totals = possDub.toString().split('').reduce<Record<string,number>>((a,el) => {
-    a[el] = (a[el] || 0) + 1;
-    return a;
-  }, {})
-  let count: number = 0;
-  for (let key in totals){
-    count++;
+  let possDub = num + 1;
+  
+  while (true) {
+    const totals = possDub.toString().split('').reduce<Record<string, number>>((a, el) => {
+      a[el] = (a[el] || 0) + 1;
+      return a;
+    }, {});
+    
+    const count = Object.keys(totals).length;
+    if (count === 2) {
+      return possDub;
+    }
+    possDub++;
   }
-  return count === 2 ? possDub : doubleton(possDub);
 }
