28bb390f2f8faf691000771571992dceb1453119
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-16 14:02:21 -0500

    handling for falsy final values

--- 7-kyu/99-problems-number-1-last-in-list/99-problems-number-1-last-in-list.js
+++ 7-kyu/99-problems-number-1-last-in-list/99-problems-number-1-last-in-list.js
@@ -1,2 +1,12 @@
- 
-const last = xs => xs[xs.length - 1] || null;
+const last = xs => {
+  if (xs.length === 0) return null;
+  const final = xs[xs.length - 1];
+  if (!final && 
+      final !== 0 && 
+      final !== undefined && 
+      final !== '' && 
+      final !== false){
+    return null;
+  }
+  return final;
+};
