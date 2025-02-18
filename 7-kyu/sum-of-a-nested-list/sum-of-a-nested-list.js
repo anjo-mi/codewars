3fbab855171133d1715fc9da6d63028bd4ab5856
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-18 14:41:43 -0500

    added function that includes recursion

--- 7-kyu/sum-of-a-nested-list/sum-of-a-nested-list.js
+++ 7-kyu/sum-of-a-nested-list/sum-of-a-nested-list.js
@@ -1,4 +1,13 @@
- 
+const sumNested = arr => {
+  if (Array.isArray(arr)){
+    return arr.reduce((a,el) => {
+      return a + sumNested(el);
+    }, 0);
+  }
+  return arr;
+};
+
+
 const sumNested = arr => {
   return arr.flat(Infinity).reduce((a,el) => a+el,0);
 };
