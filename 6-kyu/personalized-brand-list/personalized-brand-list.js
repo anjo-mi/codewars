69a29a6295e90878138064933c263f99c965dacd
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-20 15:17:40 -0500

    Update personalized-brand-list.js

--- 6-kyu/personalized-brand-list/personalized-brand-list.js
+++ 6-kyu/personalized-brand-list/personalized-brand-list.js
@@ -1,8 +1,13 @@
- 
 function sortedBrands(history){
   const totals = history.reduce((a,el) => {
     a[el.brand]= (a[el.brand] || 0) + 1;
     return a;
   }, {})
-  return Object.keys(totals).sort((a,b) => totals[b] - totals[a]);
+  return Object.keys(totals).sort((a,b) => {
+    if (totals[a] !== totals[b]){
+      return totals[b] - totals[a];
+    }else{
+      return history.findIndex(item => item.brand === a) - history.findIndex(item => item.brand === b);
+    }
+  });
 }
