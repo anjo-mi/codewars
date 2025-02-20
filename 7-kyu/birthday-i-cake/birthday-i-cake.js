4b5213d67fe32a9f8e485ff8ba330d6106800b2d
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-19 23:27:31 -0500

    Update birthday-i-cake.js

--- 7-kyu/birthday-i-cake/birthday-i-cake.js
+++ 7-kyu/birthday-i-cake/birthday-i-cake.js
@@ -1,4 +1,9 @@
 function cake(x, y){
-  const fallen = y.split('').reduce((a,el) => a + el.charCodeAt(0), 0);
-  return (fallen * 10) / x > 7 ? 'Fire!' : 'That was close!';
+  const alpha = ' abcdefghijklmnopqrstuvwxyz'
+  const fallen = y.split('').reduce((a,el,i) => {
+    if (i % 2 === 0) a += el.charCodeAt(0);
+    else a += alpha.indexOf(el);
+    return a;
+  }, 0);
+  return (fallen * 10) / x >= 7 ? 'Fire!' : 'That was close!';
 }
