66687c4f1e78ad2f64c298ed3a0ca7326a8095e8
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-22 17:18:18 -0500

    Update whats-my-golf-score.js

--- 7-kyu/whats-my-golf-score/whats-my-golf-score.js
+++ 7-kyu/whats-my-golf-score/whats-my-golf-score.js
@@ -1,7 +1,6 @@
- 
 function golfScoreCalculator(parList, scoreList){
-  if (parList.length !== scoreList.length) throw new Error('finish your round!')
-  parList = parList.split('');
-  scoreList = scoreList.split('');
-  return scoreList.reduce((a,score,i) => a = (parList[i]) - score, 0)
+  if (parList.length !== scoreList.length) throw new Error('finish your round!');
+  parList = parList.split('').map(Number);
+  scoreList = scoreList.split('').map(Number);
+  return scoreList.reduce((a,score,i) => a += (score - parList[i]), 0);
 }
