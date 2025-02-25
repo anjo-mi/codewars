6aee580197319fdaf86cb5b2edddfd9646267547
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-25 18:23:04 -0500

    Add solution - CodeHub

---
+++ 6-kyu/stringing-plus-me-plus-along/stringing-plus-me-plus-along.js
@@ -0,0 +1,12 @@
+ 
+function createMessage(firstWord) {
+  let str = [firstWord];
+  return function nextWord(word){
+    if (arguments.length === 0){
+      return str.join(' ');
+    }
+    str.push(word);
+    return nextWord;
+  }
+  return nextWord;
+}
