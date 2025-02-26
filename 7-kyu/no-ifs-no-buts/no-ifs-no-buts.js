57702aa247e61dd6ace7d790aef546ae72d63d6a
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-26 13:35:34 -0500

    Add solution - CodeHub

---
+++ 7-kyu/no-ifs-no-buts/no-ifs-no-buts.js
@@ -0,0 +1,10 @@
+ 
+function noIfsNoButs(a, b) {
+  let result = '';
+  switch (true){
+      case a > b : result = `${a} is greater than ${b}` ; break;
+      case a < b : result = `${a} is smaller than ${b}` ; break;
+      case a == b : result = `${a} is equal to ${b}` ; break;
+  }
+  return result;
+}
