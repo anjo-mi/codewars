084e91347540a47f1492e00ea40caf8ec7ade23e
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-02-27 18:38:00 -0500

    Add solution - CodeHub

---
+++ 6-kyu/clocky-mc-clock-face/clocky-mc-clock-face.js
@@ -0,0 +1,8 @@
+ 
+var whatTimeIsIt = function(angle) {
+  if (angle === 360) angle = 0;
+  let totalMinutes = angle * 2;
+  let hours = (Math.floor(totalMinutes / 60) || 12).toString().padStart(2,'0');
+  let minutes = (totalMinutes % 60).toString().padStart(2,'0');
+  return `${hours}:${minutes}`;
+}
