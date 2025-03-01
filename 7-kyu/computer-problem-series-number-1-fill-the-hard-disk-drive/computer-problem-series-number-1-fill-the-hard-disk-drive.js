e50ba08eea3d7906549fb36f8f3d91e4cfebaa29
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-03-01 01:01:54 -0500

    Update solution - CodeHub

--- 7-kyu/computer-problem-series-number-1-fill-the-hard-disk-drive/computer-problem-series-number-1-fill-the-hard-disk-drive.js
+++ 7-kyu/computer-problem-series-number-1-fill-the-hard-disk-drive/computer-problem-series-number-1-fill-the-hard-disk-drive.js
@@ -2,7 +2,7 @@
 function save(sizes, hd) {
   let i = 0;
   let total = 0;
-  while (total <= hd && sizes[i]){
+  while (total <= hd && (sizes[i] || sizes[i] === 0)){
     total += sizes[i]
     i++
   }
