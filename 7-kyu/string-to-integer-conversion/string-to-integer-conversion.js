c06ef9491e2581c78e18be18b270110541ef42b6
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-03-02 11:04:56 -0500

    Update solution - CodeHub

--- 7-kyu/string-to-integer-conversion/string-to-integer-conversion.js
+++ 7-kyu/string-to-integer-conversion/string-to-integer-conversion.js
@@ -1,8 +1,6 @@
  
 function myParseInt(str) {
-  if (!+str) return NaN
   return str.trim().includes('.') 
             ? NaN 
-            : Number(str.trim())
+            : parseInt(str.trim(),10)
 }
-​
