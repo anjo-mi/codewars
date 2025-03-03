e533877c1b835c8e3340069974eb1b4785b0cbcd
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-03-03 18:42:07 -0500

    Update solution - CodeHub

--- 7-kyu/counting-duplicates-across-multiple-lists/counting-duplicates-across-multiple-lists.js
+++ 7-kyu/counting-duplicates-across-multiple-lists/counting-duplicates-across-multiple-lists.js
@@ -1,22 +1,12 @@
 function countDuplicates(name, age, height) {
-  const objs = name.reduce((a,el,i) => {
-    const obj = {
-      name: el,
-      age: age[i],
-      height: height[i]
-    };
-    a.push(obj);
-    return a;
-  }, []);
-  const noRepeats = [];
-  for (let i = 0 ; i < objs.length ; i++){
-    const {name,age,height} = objs[i];
-    if (!(noRepeats.findIndex(el => el.name === name 
-                              && el.age === age 
-                              && el.height === height) >= 0)){
-      noRepeats.push(objs[i])
-    }
+  let dupes = 0;
+  let entries = new Set();
+  
+  for (let i = 0 ; i < name.length ; i++){
+    const entry = [ name[i] , age[i] , height[i] ].join(',');
+    if (entries.has(entry)) dupes++
+    else entries.add(entry);
   }
-  return objs.length - noRepeats.length;
+  return dupes;
 }
 ​
