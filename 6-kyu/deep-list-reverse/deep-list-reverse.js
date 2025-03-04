8c7d38213a1e932a4daca31f4b778e9517fffec5
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-03-04 14:41:27 -0500

    Update solution - CodeHub

--- 6-kyu/deep-list-reverse/deep-list-reverse.js
+++ 6-kyu/deep-list-reverse/deep-list-reverse.js
@@ -1,11 +1,5 @@
+ 
 function deepReverse(l){
-  const revList = [];
-  for (let i = l.length - 1 ; i >= 0 ; i--){
-    if ( Array.isArray(l[i]) ){
-      revList.push( deepReverse(l[i]) )
-    }else{
-      revList.push( l[i] )
-    }
-  }
-  return revList;
+  if (!Array.isArray(l)) return l;
+  return l.map(deepReverse).reverse();
 }
