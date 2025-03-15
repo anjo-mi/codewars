b7a61b74bf10fe97cb2868abfd028763cf428b66
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-03-14 20:32:10 -0400

    Update solution - CodeHub

--- 7-kyu/number-climber/number-climber.js
+++ 7-kyu/number-climber/number-climber.js
@@ -1,7 +1,5 @@
  
 function climb(n){
   if (n === 1) return [1];
-  return climb(n % 2 === 0 
-               ? n / 2 
-               : (n-1) / 2).concat(n);
+  return climb(Math.floor(n/2)).concat(n);
 }
