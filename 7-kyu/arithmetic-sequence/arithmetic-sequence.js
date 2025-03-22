ebbf8d7d83c9a609f9d040164883583c1ea26eb3
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-03-21 21:54:33 -0400

    Update solution - CodeHub

--- 7-kyu/arithmetic-sequence/arithmetic-sequence.js
+++ 7-kyu/arithmetic-sequence/arithmetic-sequence.js
@@ -1,9 +1,2 @@
  
-function nthterm(first, n, c){
-  const seq = [];
-  for (let i = 0 ; i <= n ; i++){
-    if (i === 0) seq[i] = first
-    else {seq[i] = seq[i-1] + c}
-  }
-  return seq[n];
-}
+const nthterm = (first, n, c) => first + n * c;
