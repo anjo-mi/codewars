5c621b1a32d84b43d4a6289cc33f995a64f1ca37
anjo-mi <148298717+anjo-mi@users.noreply.github.com>
2025-03-28 11:25:09 -0400

    Update solution - CodeHub

--- 6-kyu/string-subpattern-recognition-i/string-subpattern-recognition-i.js
+++ 6-kyu/string-subpattern-recognition-i/string-subpattern-recognition-i.js
@@ -1,13 +1,5 @@
- 
 function hasSubpattern(str){
-  console.log(str)
-  for (let i = 1 ; i < Math.ceil(str.length / 2) ; i++){
-    let part = str.slice(0,i);
-    while (part.length <= str.length){
-      if (part !== str.slice(0,part.length)) break;
-      if (part === str) return true;
-      part += str.slice(0,i);
-    }
-  }
-  return false;
+  if (str.length <= 1) return false;
+  const double = str + str;
+  return double.indexOf(str,1) < str.length;
 }
