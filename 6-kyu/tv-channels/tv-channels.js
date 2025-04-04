57119bb27d1c56c3fb997777b3ff5e26146d59c0
Kyle Nojaim <nojaim.k@gmail.com>
2025-04-04 08:27:47 -0400

    Update solution - CodeHub

--- 6-kyu/tv-channels/tv-channels.js
+++ 6-kyu/tv-channels/tv-channels.js
@@ -1,10 +1,2 @@
  
-function redarr(arr) {
-  let sortedChannels = arr.slice(0).sort();
-  sortedChannels = Array.from(new Set(sortedChannels));
-  const obj = {};
-  for (let i = 0; i < sortedChannels.length ; i++){
-    obj[i] = sortedChannels[i];
-  }
-  return obj;
-}
+const redarr = (arr) => Object.assign({}, Array.from(new Set(arr)).sort());
