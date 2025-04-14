c36870329b0c6111cfc1dca43cfcb454b64248fe
Kyle Nojaim <nojaim.k@gmail.com>
2025-04-14 17:38:17 -0400

    Update solution - CodeHub

--- 7-kyu/all-none-and-any/all-none-and-any.js
+++ 7-kyu/all-none-and-any/all-none-and-any.js
@@ -1,21 +1,12 @@
  
 Array.prototype.all = function (p) {
-  for (let i = 0 ; i < this.length ; i++){
-    if (!p(this[i])) return false;
-  }
-  return true;
+  return this.filter(p).length === this.length;
 };
 ​
 Array.prototype.none = function (p) {
-  for (let i = 0 ; i < this.length ; i++){
-    if (p(this[i])) return false;
-  }
-  return true;
+  return this.filter(p).length === 0;
 };
 ​
 Array.prototype.any = function (p) {
-  for (let i = 0 ; i < this.length ; i++){
-    if (p(this[i])) return true;
-  }
-  return false;
+  return this.filter(p).length > 0;
 };
