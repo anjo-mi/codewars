f186f2221b5df55ec8c4f0eb1a757fce3f06cc19
Kyle Nojaim <nojaim.k@gmail.com>
2025-04-26 17:22:46 -0400

    Update solution - CodeHub

--- 7-kyu/train-to-remove-duplicates-from-an-array-with-filter/train-to-remove-duplicates-from-an-array-with-filter.js
+++ 7-kyu/train-to-remove-duplicates-from-an-array-with-filter/train-to-remove-duplicates-from-an-array-with-filter.js
@@ -1,8 +1,4 @@
  
 function unique(arr) {
-  const filtered = [];
-  arr.filter(el => {
-    if (!filtered.includes(el)) filtered.push(el)
-  })
-  return filtered;
+  return arr.filter((el,i) => arr.indexOf(el) === i)
 }
