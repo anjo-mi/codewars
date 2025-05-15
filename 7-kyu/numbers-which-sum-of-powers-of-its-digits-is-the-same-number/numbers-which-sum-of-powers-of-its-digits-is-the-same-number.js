f009129fdc10ce1630f589b82f10e9e5c6551569
Kyle Nojaim <nojaim.k@gmail.com>
2025-05-15 10:30:32 -0400

    Add solution - CodeHub

---
+++ 7-kyu/numbers-which-sum-of-powers-of-its-digits-is-the-same-number/numbers-which-sum-of-powers-of-its-digits-is-the-same-number.js
@@ -0,0 +1,11 @@
+ 
+const sumOfExps = (n,e) => n.toString().split('').map(Number).reduce((a,el) => a + (el**e),0);
+function eqSumPowdig(hMax, exp) {
+  const results = [];
+  const exped = sumOfExps(hMax,exp);
+  for (let i = 2 ; i <= hMax ; i++){
+    if (i === sumOfExps(i,exp)) results.push(i);
+  }
+  return results;
+}
+​
