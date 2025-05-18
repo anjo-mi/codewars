a3c6e70e6773daaceff045af095ff4e320ac49fb
Kyle Nojaim <nojaim.k@gmail.com>
2025-05-18 18:53:08 -0400

    Update solution - CodeHub

--- 7-kyu/most-valuable-character/most-valuable-character.js
+++ 7-kyu/most-valuable-character/most-valuable-character.js
@@ -1,26 +1,15 @@
  
 function solve(s) {
-  const diff = {};
-  for (let i = 0 ; i < s.length ; i++){
-    if (!diff[s[i]]){
-      diff[s[i]] = {first: i, last:null};
-    }
-    else if(diff[s[i]]){
-      diff[s[i]].last = i;
-    } 
-  }
   let maxDiff = -Infinity;
   let maxDiffChar = null;
-  for (const char in diff){
-    const c = diff[char]
-    if ((c.first && c.last) || (c.first === 0 && c.last)){
-      if (c.last - c.first > maxDiff){
-        maxDiff = c.last - c.first;
-        maxDiffChar = char;
-      }else if (c.last - c.first === maxDiff && char.charCodeAt(0) < maxDiffChar.charCodeAt(0)){
-        maxDiff = c.last - c.first;
-        maxDiffChar = char;
-      }
+  for (const char of s){
+    const diff = s.lastIndexOf(char) - s.indexOf(char);
+    if (diff > maxDiff){
+      maxDiff = diff;
+      maxDiffChar = char;
+    }else if(diff === maxDiff && char.charCodeAt(0) < maxDiffChar.charCodeAt(0)){
+      maxDiff = diff;
+      maxDiffChar = char;
     }
     if (!maxDiffChar){
       maxDiffChar = char;
