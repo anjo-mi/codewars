bc4115173e0681d604d594c7086b9440c00b7c82
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-04 11:40:19 -0400

    Add solution - CodeHub

---
+++ 6-kyu/patterncraft-strategy/patterncraft-strategy.js
@@ -0,0 +1,23 @@
+ 
+class Fly {
+  move(unit) {
+    unit.position += 10;
+  }
+}
+​
+class Walk {
+  move(unit) {
+    unit.position += 1;
+  }
+}
+​
+class Viking {
+  constructor() {
+    this.moveBehavior = new Walk();
+    this.position = 0;
+  }
+  
+  move() {
+    this.moveBehavior.move(this);
+  }
+}
