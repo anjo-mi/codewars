268f44aa4fbc267ed96a5461649d1d0d1b78db8e
Kyle Nojaim <nojaim.k@gmail.com>
2025-08-08 22:10:12 -0400

    Add solution - CodeHub

---
+++ 8-kyu/configure-package-json-for-a-node-application/configure-package-json-for-a-node-application.js
@@ -0,0 +1,14 @@
+ 
+const configuration = {
+  "name": "tools",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+};
+// Hint: try creating with `npm init` command
+// Help! -> https://docs.npmjs.com/files/package.json
