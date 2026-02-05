d07eb87f2a766d4112f7d8e7f79d12bc16464c78
Kyle Nojaim <nojaim.k@gmail.com>
2026-02-04 19:17:58 -0500

    Add solution - CodeHub

---
+++ 8-kyu/playing-with-cubes-i/playing-with-cubes-i.java
@@ -0,0 +1,11 @@
+ 
+public class Cube{
+  int side = 0;
+  public void setSide(int arg){
+    this.side = arg;
+  }
+  public int getSide(){
+    System.out.println(this.side);
+    return this.side;
+  }
+}
