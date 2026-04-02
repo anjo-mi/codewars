7cbda6358f8f62a55802f29dd6cfbf2884b5996e
Kyle Nojaim <nojaim.k@gmail.com>
2026-04-02 17:47:09 -0400

    Update solution - CodeHub

--- 5-kyu/the-hunger-games-zoo-disaster/the-hunger-games-zoo-disaster.js
+++ 5-kyu/the-hunger-games-zoo-disaster/the-hunger-games-zoo-disaster.js
@@ -1,36 +1,4 @@
-var whoEatsWho = function(zoo) {
-  const eats = {
-    antelope: {grass: 'antelope eats grass'},
-    ['big-fish']: {['little-fish']: 'big-fish eats little-fish'},
-    bug: {leaves: 'bug eats leaves'},
-    bear: {
-      ['big-fish']: 'bear eats big-fish',
-      bug: 'bear eats bug',
-      chicken: 'bear eats chicken',
-      cow: 'bear eats cow',
-      leaves: 'bear eats leaves',
-      sheep: 'bear eats sheep'
-    },
-    chicken: {bug: 'chicken eats bug'},
-    cow: {grass: 'cow eats grass'},
-    fox: {
-      chicken: 'fox eats chicken',
-      sheep: 'fox eats sheep',
-    },
-    giraffe: {leaves: 'giraffe eats leaves'},
-    lion: {
-      antelope: 'lion eats antelope',
-      cow: 'lion eats cow'
-    },
-    panda: {leaves: 'panda eats leaves'},
-    sheep: {grass: 'sheep eats grass'}
-  };
-  const newZoo = [zoo];
-  let animals = zoo.split(',');
-  
-  function canEat(animal, food){
-    return eats[animal] && eats[animal][food];
-  }
+ 
   
   let somethingEats = true;
   while (somethingEats){
@@ -58,4 +26,5 @@ var whoEatsWho = function(zoo) {
   }
   
   return [...newZoo,animals.join(',')];
-}​
+}
+​
