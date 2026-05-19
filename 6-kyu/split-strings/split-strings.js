448447fdb4a0d45cdd050d8352a70ee114c18e9d
Kyle Nojaim <nojaim.k@gmail.com>
2026-05-19 17:58:32 -0400

    Update solution - CodeHub

--- 6-kyu/split-strings/split-strings.js
+++ 6-kyu/split-strings/split-strings.js
@@ -1,9 +1,9 @@
  
-function solution(str){
+function solution(str, step = 2){
   const res = [];
-  for (let i = 0 ; i < str.length ; i += 2){
-    let portion = str.slice(i,i+2);
-    if (portion.length === 1) portion += '_';
+  for (let i = 0 ; i < str.length ; i += step){
+    let portion = str.slice(i,i+step);
+    while (portion.length < step) portion += '_';
     res.push(portion)
   }
   return res;
