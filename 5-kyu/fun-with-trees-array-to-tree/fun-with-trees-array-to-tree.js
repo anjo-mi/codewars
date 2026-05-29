8e5ec96d0a98595b6f18e421156b8e0c915b6954
Kyle Nojaim <nojaim.k@gmail.com>
2026-05-29 00:14:03 -0400

    Update solution - CodeHub

--- 5-kyu/fun-with-trees-array-to-tree/fun-with-trees-array-to-tree.js
+++ 5-kyu/fun-with-trees-array-to-tree/fun-with-trees-array-to-tree.js
@@ -1,19 +1,5 @@
  
-function arrayToTree(array) {
-  if (!array.length) return undefined;
-  const nodes = [ new TreeNode( array[0] ) ];
-  let i = 0;
-  while (i < array.length){
-    let leftInd = 2 * i + 1;
-    let rightInd = 2 * i + 2;
-    const left = leftInd < array.length ? new TreeNode(array[leftInd]) : null;
-    const right = rightInd < array.length ? new TreeNode(array[rightInd]) : null;
-    if (left) nodes[i].left = left;
-    if (right) nodes[i].right = right;
-    nodes.push(nodes[i].left);
-    nodes.push(nodes[i].right);
-    i++;
-  }
-  return nodes[0];
+function arrayToTree(a,i=0) {
+  if (i >= a.length) return undefined;
+  return new TreeNode( a[i] , arrayToTree( a , 2*i+1 ) , arrayToTree( a, 2*i+2 ) );
 };
-​
