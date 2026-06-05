449ce0c785e68ad1feec6cf8131c9a1328c23161
Kyle Nojaim <nojaim.k@gmail.com>
2026-06-04 21:23:19 -0400

    Update solution - CodeHub

--- 7-kyu/multiples-by-permutations-ii/multiples-by-permutations-ii.js
+++ 7-kyu/multiples-by-permutations-ii/multiples-by-permutations-ii.js
@@ -1,26 +1,7 @@
  
-const compareDigits = (n1,n2) => {
-  const digs1 = n1.toString().split('').reduce((a,el,i) => {
-    if (!a[el]) a[el] =0;
-    a[el]++;
-    return a;
-  },{})
-  const digs2 = n2.toString().split('').reduce((a,el,i) => {
-    if (!a[el]) a[el] = 0;
-    a[el]++;
-    return a;
-  },{})
-  for (const [key,val] of Object.entries(digs1)){
-    if (!(key in digs2) || digs2[key] !== val) return false;
-  }
-  for (const [key,val] of Object.entries(digs2)){
-    if (!(key in digs1) || digs1[key] !== val) return false;
-  }
-  return true;
-}
-​
 function findLowestInt(k) {
+  const stringIt = n => n.toString().split('').sort().join('');
   let x = 1;
-  while (!compareDigits(k*x, (k+1) * x)) x++;
+  while (stringIt(k * x) !== stringIt((k+1) * x)) x++;
   return x;
 }
