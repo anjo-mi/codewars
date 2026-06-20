f8f5329fcc8e69c6ab25ab340047c9ab060694fe
Kyle Nojaim <nojaim.k@gmail.com>
2026-06-20 19:19:21 -0400

    Update solution - CodeHub

--- 6-kyu/pyramid-array/pyramid-array.ts
+++ 6-kyu/pyramid-array/pyramid-array.ts
@@ -1,10 +1,6 @@
  
 export function pyramid(n: number) : Array<Array<Number>> {
   const res:number[][] = [];
-  while (n > 0){
-    const insert:number[] = Array(n).fill(1);    
-    res[n-1] = insert;
-    n--;
-  }
+  while (n > 0) res[n-1] = Array(n--).fill(1);
   return res;
 }
