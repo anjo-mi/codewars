3b8576351f494b65b05ce7c938cfef7bc3aba4a8
Kyle Nojaim <nojaim.k@gmail.com>
2026-07-10 10:53:44 -0400

    Update solution - CodeHub

--- 7-kyu/number-manipulation-i-easy/number-manipulation-i-easy.js
+++ 7-kyu/number-manipulation-i-easy/number-manipulation-i-easy.js
@@ -1,10 +1,6 @@
  
-function manipulate(num) { 
-  const mid = Math.floor(num.toString().length / 2);
-  const convert = num.toString().split('').fill('0',mid).map(Number);
-  let res = 0;
-  for (let i = convert.length - 1 ; i >= 0 ; i--){
-    res += convert[i] * 10**((convert.length - 1) - i)
-  }
-  return res;
-}
+function manipulate(num) {
+  const length = num.toString().length
+  const mid = Math.floor(length / 2);
+  return parseInt(num.toString().slice(0,mid) + '0'.repeat(length - mid));
+} 
