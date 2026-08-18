abf8ae047562b0d2541631ccd8978b74668cc5c2
Kyle Nojaim <nojaim.k@gmail.com>
2026-08-18 19:31:25 -0400

    Add solution - CodeHub

---
+++ 8-kyu/count-of-positives-slash-sum-of-negatives/count-of-positives-slash-sum-of-negatives.ts
@@ -0,0 +1,9 @@
+export function countPositivesSumNegatives(input: number[] | null): number[] {
+  if (!input || !input.length) return [];
+  const res:[number,number] = [0,0];
+  for (const num of input){
+    if (num > 0) res[0]++;
+    else res[1] += num;
+  }
+  return res;
+}
