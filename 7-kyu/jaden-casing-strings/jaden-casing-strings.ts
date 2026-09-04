99c56f6947fa2ba981eb2c12dc436ac97a86740d
Kyle Nojaim <nojaim.k@gmail.com>
2026-09-04 03:53:01 -0400

    Add solution - CodeHub

---
+++ 7-kyu/jaden-casing-strings/jaden-casing-strings.ts
@@ -0,0 +1,9 @@
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map(word => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+}
+​
+​
+interface String {      // Declaration needed, don't remove it
+  toJadenCase(): string;
+}
+​
